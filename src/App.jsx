31256fb97cb7e22f910d002305e8793d9d972914
HiwatashiYuko <sarappi3.72kero4@gmail.com>
2023-05-02 06:41:25 +0000

    Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
-import "./styles.css";
+import React from "react";
+import "./style.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+export const App = () => {
+
+    //TODOリストの移動にはuseStateを使って実装する。
+    const [incompleteTodos, setIncompleteTodos] = useState(["入力されたTODO1", "入力されたTODO2"]);
+    return (
+        <>
+            <div className="input-area">
+                <input placeholder="TODOを入力" />
+                <button>追加</button>
+            </div>
+            <div className="incomplete-area">
+                <p className="title">未完了のTODO</p>
+                <ul>
+                    { incompleteTodos.map(() => {
+                        
+                    }) }
+
+                    <div className="list-row">
+                    <li>入力されたTODO1</li>
+                    <button>完了</button>
+                    <button>削除</button>
+                    </div className="list-row">
+                    <div>
+                    <li>入力されたTODO2</li>
+                    <button>完了</button>
+                    <button>削除</button>
+                    </div>
+                </ul>
+
+            </div>
+            <div className="complete-area">
+            <p className="title">完了したTODO</p>
+                <ul>
+                    <div className="list-row">
+                    <li>完了TODO1</li>
+                    <button>戻す</button>
+                    </div>
+                   
+                </ul>
+            </div>
+        </>
+    );
+};
+
